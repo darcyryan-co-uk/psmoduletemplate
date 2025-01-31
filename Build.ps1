ad43b2178c5f4468ae31b2a79721aec569703992
jdarcyryan <129673679+jdarcyryan@users.noreply.github.com>
2025-01-31 20:00:24 +0000

    build.ps1

--- Build.ps1
+++ Build.ps1
@@ -1,112 +1,83 @@
 param(
-
-    [Parameter(Mandatory)]
-    [ValidateNotNullOrEmpty()]
-    [ValidateScript({
-            !($_ -contains " ")
-        })]
     [string]
     $Name,
     
-    [ValidateNotNull()]
-    [ValidateScript({
-            !($_ -contains "`n")
-        })]
-    [string]
-    $Description = "",
+    $Description = '',
     
-    [ValidateNotNullOrEmpty()]
     [version]
-    $Version = "1.0",
+    $Version = 1.0,
 
-    [ValidateNotNullOrEmpty()]
     [string]
-    $Author = "$env:UserDomain\$env:UserName",
+    $Author = $env:USERNAME,
 
-    [ValidateNotNullOrEmpty()]
     [string]
-    $Company = "DarcyRyan",
+    $Company = 'DR',
 
-    [ValidateNotNull()]
     [guid]
     $GUID = (New-Guid),
 
-    [ValidateNotNull()]
     [datetime]
     $DateTime = (Get-Date),
 
-    [ValidateNotNullOrEmpty()]
-    [ValidateScript({
-            (Test-Path $_ -PathType Container)
-        })]
     [string]
     $OutputDirectory = $PWD.Path
-
 )
 
-function Replace-Multiple {
+function ConvertTo-Replaced {
     param(
-
-        [Parameter(Mandatory, ValueFromPipeline)]
+        [Parameter(ValueFromPipeline)]
         [string]
-        $InputObject,
+        $Source,
 
         [hashtable]
-        $Replace
-
+        $Index
     )
 
-    $Replace.Keys.split() | foreach {
-        $InputObject = $InputObject -Replace $_, $Replace.$_
+    $Index.Keys | foreach {
+        $Source = $Source -replace $_, $Replace.$_
     }
 
-    return $InputObject
+    return $Source
 }
 
 $regex = @{
 
-    "{Name}"        = $Name
-    "{Description}" = $Description
-    "{Version}"     = $Version.ToString()
-    "{Author}"      = $Author
-    "{Company}"     = $Company
-    "{GUID}"        = $GUID.Guid
-    "{Date}"        = $DateTime | Get-Date -Format "dd/MM/yyyy"
-    "{Year}"        = $DateTime | Get-Date -Format "yyyy"
+    '{Name}'        = $Name
+    '{Description}' = $Description
+    '{Version}'     = $Version.ToString()
+    '{Author}'      = $Author
+    '{Company}'     = $Company
+    '{GUID}'        = $GUID.Guid
+    '{Date}'        = $DateTime | Get-Date -Format 'dd/MM/yyyy'
+    '{Year}'        = $DateTime | Get-Date -Format 'yyyy'
 
 }
 
 $targetFiles = @(
-
     "$PSScriptRoot\{Name}.psd1"
     "$PSScriptRoot\{Name}.psm1"
-
 )
 
-$moduleDirectory = "$OutputDirectory\$Name\$($regex."{Version}")"
+$moduleDirectory = "$OutputDirectory\$Name\$($regex.'{Version}')"
 
 if (Test-Path $moduleDirectory) {
     Write-Error "unable to overwrite '$moduleDirectory'." -ErrorAction Stop
 }
 
 New-Item -Path $moduleDirectory -ItemType Directory | Out-Null
-@(
-
-    "Public"
-    "Private"
-
-) | foreach {
+'Public', 'Private', 'Classes' | foreach {
     New-Item -Path "$moduleDirectory\$_" -ItemType Directory | Out-Null
 }
+
 $targetFiles | Copy-Item -Destination $moduleDirectory
 $targetFiles = $targetFiles | foreach {
     "$moduleDirectory\$(Get-Item $_ | % Name)"
 }
 
 $targetFiles | foreach {
-    $content = Get-Content -Path $_ -Raw | Replace-Multiple -Replace $regex
+    $content = Get-Content -Path $_ -Raw | ConvertTo-Replaced -Index $regex
     $content | Set-Content -Path $_
 
-    $name = Get-Item -Path $_ | % Name | Replace-Multiple -Replace $regex
+    $name = Get-Item -Path $_ | % Name | ConvertTo-Replaced -Index $regex
     $_ | Rename-Item -NewName $name
 }
