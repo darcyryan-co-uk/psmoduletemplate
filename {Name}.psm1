bd234e37a67b3ec89642888be93279b0c3d3c28e
jdarcyryan <129673679+jdarcyryan@users.noreply.github.com>
2025-01-31 20:11:36 +0000

    typing improvements

--- {Name}.psm1
+++ {Name}.psm1
@@ -1,12 +1,12 @@
-$moduleName = (Get-Item "$PSScriptRoot\..").Name.Split("\")[-1]
+$moduleName = Get-Item $PSScriptRoot | % Parent | % BaseName
 $moduleManifest = "$PSScriptRoot\$moduleName.psd1"
 $publicFunctionsPath = "$PSScriptRoot\Public"
 $privateFunctionsPath = "$PSScriptRoot\Private"
 $currentManifest = Test-ModuleManifest $moduleManifest
 
 $aliases = @()
-$publicFunctions = Get-ChildItem $publicFunctionsPath -Recurse | ? { ! $_.PSIsContainer } | ? { $_.Extension -eq ".ps1" }
-$privateFunctions = Get-ChildItem $privateFunctionsPath -Recurse | ? { ! $_.PSIsContainer } | ? { $_.Extension -eq ".ps1" }
+$publicFunctions = Get-ChildItem $publicFunctionsPath -Recurse -File | ? Extension -eq '.ps1'
+$privateFunctions = Get-ChildItem $privateFunctionsPath -Recurse -File | ? Extension -eq '.ps1'
 $publicFunctions | foreach { 
     . $_.FullName
 
@@ -24,7 +24,7 @@ $privateFunctions | foreach {
     . $_.FullName
 }
 
-$functionsAdded = $publicFunctions | ? { $_.BaseName -notin $currentManifest.ExportedFunctions.Keys }
+$functionsAdded = $publicFunctions | ? BaseName -notin $currentManifest.ExportedFunctions.Keys
 $functionsRemoved = $currentManifest.ExportedFunctions.Keys | ? { $_ -notin $publicFunctions.BaseName }
 $aliasesAdded = $aliases | ? { $_ -notin $currentManifest.ExportedAliases.Keys }
 $aliasesRemoved = $currentManifest.ExportedAliases.Keys | ? { $_ -notin $aliases }
